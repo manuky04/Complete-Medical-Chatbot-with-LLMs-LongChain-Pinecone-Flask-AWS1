e31fae635d08f403fb2ac7924bfe160bff113efe
mantu pandit <manuky04@gmail.com>
2026-07-23 10:58:32 -0700

    requirements.added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    auther="Mantu kumar Pandit",
+    auther_email="manuky04@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
