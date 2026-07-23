c6952aabf0ef515e4e737862c297c59e05a76718
mantu pandit <manuky04@gmail.com>
2026-07-23 10:15:53 -0700

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating Directory
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Directory and files created successfully!."
+
